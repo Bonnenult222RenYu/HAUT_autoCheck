d0dcb421ea832220516274cc15e107532ddbbee6
Bonnenult222RenYu <46435339+Bonnenult222RenYu@users.noreply.github.com>
2020-11-18 19:22:27 +0800

    Update main.py

--- main.py
+++ main.py
@@ -73,7 +73,7 @@ def getUserJson(userInfo,token):
     temperature = round(a, 1)
     return  {
         "businessType": "epmpics",
-        "method": "submitUpInfoSchool",
+        "method": "submitUpInfo",
         "jsonData": {
         "deptStr": {
             "deptid": userInfo['classId'],
@@ -85,11 +85,12 @@ def getUserJson(userInfo,token):
         "customerid": userInfo['customerId'],
         "deptid": userInfo['classId'],
         "source": "app",
-        "templateid": "clockSign2",
+        "templateid": "pneumonia",
         "stuNo": userInfo['stuNo'],
         "username": userInfo['username'],
+        "phonenum": "",
         "userid": round(time.time()),
-        "updatainfo": [  
+        "updatainfo": [
             {
                 "propertyname": "temperature",
                 "value": temperature
@@ -97,13 +98,75 @@ def getUserJson(userInfo,token):
             {
                 "propertyname": "symptom",
                 "value": "无症状"
+            },
+            {
+                "propertyname": "isConfirmed",
+                "value": "否"
+            },
+            {
+                "propertyname": "isdefinde",
+                "value": "否.未隔离"
+            },
+            {
+                "propertyname": "isGoWarningAdress",
+                "value": "否"
+            },
+            {
+                "propertyname": "isTouch",
+                "value": "否"
+            },
+            {
+                "propertyname": "isFFHasSymptom",
+                "value": "没有"
+            },
+            {
+                "propertyname": "isContactFriendIn14",
+                "value": "没有"
+            },
+            {
+                "propertyname": "xinqing",
+                "value": "健康"
+            },
+            {
+                "propertyname": "bodyzk",
+                "value": "是"
+            },
+            {
+                "propertyname": "cxjh",
+                "value": "否"
+            },
+            {
+                "propertyname": "isleaveaddress",
+                "value": "否"
+            },
+            {
+                "propertyname": "gtjz0511",
+                "value": "否"
+            },
+            {
+                "propertyname": "medicalObservation",
+                "value": "绿色"
+            },
+            {
+                "propertyname": "ownPhone",
+                "value": "13133966922"
+            },
+            {
+                "propertyname": "emergencyContact",
+                "value": "梁必柜"
+            },
+            {
+                "propertyname": "mergencyPeoplePhone",
+                "value": "13970603959"
+            },
+            {
+                "propertyname": "assistRemark",
+                "value": ""
             }
         ],
-        "customerAppTypeRuleId": 147,
-        "clockState": 0,
+        "gpsType": 1,
         "token": token
-        },
-        "token": token
+        }
     }    
 
 #信息获取函数
